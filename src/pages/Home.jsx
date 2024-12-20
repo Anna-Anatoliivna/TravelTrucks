3445b29b2c5c0e0d87d2fb921ea0e159199e290c
Anna <iaanya27@gmail.com>
2024-12-20 11:18:01 +0200

    added styles for Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import { useNavigate } from 'react-router-dom';
+import css from './Home.module.css';
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Campers of your dreams</h1>
-    
+    <div className={css.home}>
+      <h1 className={css.title}>Campers of your dreams</h1>
+      <h2 className={css.subtitle}>
+        You can find everything you want in our catalog
+      </h2>
       <button onClick={() => navigate('/catalog')}>View Now</button>
     </div>
   );
